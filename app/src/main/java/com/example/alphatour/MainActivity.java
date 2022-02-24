00636094854e17421157069cce1c6b3cba553784
Daniele De Santis <desantisdaniele@hotmail.com>
2022-02-24 18:58:05 +0100

    commit di prova

--- app/src/main/java/com/example/alphatour/MainActivity.java
+++ app/src/main/java/com/example/alphatour/MainActivity.java
@@ -10,8 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        /*commento prova*/
-        /*secondo commento prova*/
+
+
 
     }
 }
