f46a8ea754a602d5fd2537881cd620cd7205613a
Francesco Pio Di Giorgio <dgfrancesco12@gmail.com>
2022-02-26 17:05:01 +0100

    Creazione delle activity Welcome, Login e Register e i relativi file di layout

---
+++ app/src/main/java/com/example/alphatour/LoginActivity.java
@@ -0,0 +1,19 @@
+package com.example.alphatour;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+}
