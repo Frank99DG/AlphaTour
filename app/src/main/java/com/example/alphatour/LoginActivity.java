d583a74af65bcf6264ebcf8c32c78667ed1aa739
Christian Cardinale <cardy.chry@libero.it>
2022-02-28 22:41:19 +0100

    Impostato timer a WelcomeActivity Reso interattivo l'editView di Email Impostata scrollabile la RegisterActivity Aggiunta la funzione openRegisterActivity

--- app/src/main/java/com/example/alphatour/LoginActivity.java
+++ app/src/main/java/com/example/alphatour/LoginActivity.java
@@ -2,7 +2,9 @@ package com.example.alphatour;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -15,5 +17,11 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onPause() {
         super.onPause();
+    }
+
+
+    public void openRegisterActivity(View v){
+        Intent intent = new Intent(this, RegisterActivity.class);
+        startActivity(intent);
     }
 }
