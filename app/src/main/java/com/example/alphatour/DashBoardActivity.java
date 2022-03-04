97c794ce5d63c5f29e4acdd80b7d1afd2be98765
Giuseppe Barbone <giuseppe93barbone@gmail.com>
2022-03-04 17:40:53 +0100

    modifica file activity_dash_board.xml, AndroidManifest.xml, DashBoardActivity.java, ic_dashBoard.png, ic_dashBoard.xml, misc.xml, strings.xml

---
+++ app/src/main/java/com/example/alphatour/DashBoardActivity.java
@@ -0,0 +1,14 @@
+package com.example.alphatour;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DashBoardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dash_board);
+    }
+}
