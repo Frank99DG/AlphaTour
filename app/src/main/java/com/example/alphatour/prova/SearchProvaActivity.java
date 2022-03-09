f5e873c4ea52bddb160c4c59bc57c5cbc97adba0
Francesco Pio Di Giorgio <dgfrancesco12@gmail.com>
2022-03-09 17:24:01 +0100

    Creazione del package di prova e dell'activity di ricerca

---
+++ app/src/main/java/com/example/alphatour/prova/SearchProvaActivity.java
@@ -0,0 +1,16 @@
+package com.example.alphatour.prova;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.alphatour.R;
+
+public class SearchProvaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_prova);
+    }
+}
