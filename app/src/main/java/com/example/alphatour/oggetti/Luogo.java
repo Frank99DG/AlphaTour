c6b8a8f7915bc78c02e06f20af4c583ad32a784f
Christian Cardinale <cardy.chry@libero.it>
2022-03-10 19:31:24 +0100

    Riempimento classi Luogo e Zona , importazione libreria JGraphT

--- app/src/main/java/com/example/alphatour/oggetti/Luogo.java
+++ app/src/main/java/com/example/alphatour/oggetti/Luogo.java
@@ -1,18 +1,23 @@
 package com.example.alphatour.oggetti;
+import org.jgrapht.*;
+import org.jgrapht.graph.DefaultEdge;
+import org.jgrapht.graph.SimpleDirectedGraph;
 
 public class Luogo {
 
     public String nome,citta,tipo;
+    public Graph<Zona,DefaultEdge> zoneLuogo = new SimpleDirectedGraph<>(DefaultEdge.class);
+
 
     public Luogo(){
 
     }
 
-    public Luogo(String nome, String citta, String tipo){
+    public Luogo(String nome, String citta, String tipo, Graph zoneLuogo){
         this.nome = nome;
         this.citta = citta;
         this.tipo = tipo;
-
+        this.zoneLuogo = zoneLuogo;
     }
 
 
--- app/src/main/java/com/example/alphatour/oggetti/Zona.java
+++ app/src/main/java/com/example/alphatour/oggetti/Zona.java
@@ -1,4 +1,22 @@
 package com.example.alphatour.oggetti;
 
+import org.jgrapht.Graph;
+import org.jgrapht.graph.DefaultEdge;
+import org.jgrapht.graph.SimpleDirectedGraph;
+import org.jgrapht.graph.SimpleGraph;
+
 public class Zona {
+    public String nome;
+    public Graph<Elemento,DefaultEdge> elementiZona = new SimpleGraph<>(DefaultEdge.class);
+
+    public Zona(){
+
+    }
+
+    public Zona(String nome, Graph elementiZona){
+        this.nome = nome;
+        this.elementiZona = elementiZona;
+
+
+    }
 }
