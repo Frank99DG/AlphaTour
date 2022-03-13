6d09fc3629c3cf5a5c5d734600d1e8c4ca35f385
Francesco Pio Di Giorgio <dgfrancesco12@gmail.com>
2022-03-13 19:45:14 +0100

    Creazione dell'activity activity_dash_board_curatore_seria.xml e dei file ic_add_luogo.xml, ic_arrow_right.xml,from_bottom_animation, rotate_open_animation.xml, rotate_close_animation.xml, to_bottom_animation.xml . Modifica dei file AndroidManifest.xml,

---
+++ app/src/main/java/com/example/alphatour/prova/DashBoardCuratore_seria.java
@@ -0,0 +1,71 @@
+package com.example.alphatour.prova;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.EditText;
+
+
+import com.example.alphatour.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class DashBoardCuratore_seria extends AppCompatActivity {
+
+    FloatingActionButton bottone;
+    EditText nomeLuogo;
+    private   Animation rotateOpen;
+    private   Animation rotateClose;
+    private   Animation fromBottom;
+    private   Animation toBottom;
+    private  boolean clicked=false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dash_board_curatore_seria);
+
+        bottone=findViewById(R.id.addLuogo);
+        nomeLuogo=findViewById(R.id.textNomeLuogo);
+        rotateOpen= AnimationUtils.loadAnimation(this,R.anim.rotate_open_animation);
+        rotateClose= AnimationUtils.loadAnimation(this,R.anim.rotate_close_animation);
+        fromBottom= AnimationUtils.loadAnimation(this,R.anim.from_bottom_animation);
+        toBottom= AnimationUtils.loadAnimation(this,R.anim.to_bottom_animation);
+    }
+
+
+
+    public void aggiungiLuogo(View v){
+
+        setVisibility(clicked);
+        setAnimation(clicked);
+        clicked=!clicked; //stessa cosa che fare if then else
+
+    }
+
+    private void setVisibility(boolean clicked) {
+
+        if(!clicked){
+            nomeLuogo.setVisibility(View.VISIBLE);
+        }else{
+            nomeLuogo.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    private void setAnimation(boolean clicked) {
+
+        if(!clicked){
+            bottone.startAnimation(rotateOpen);
+            nomeLuogo.startAnimation(fromBottom);
+
+        }else{
+            nomeLuogo.startAnimation(toBottom);
+            bottone.startAnimation(rotateClose);
+
+        }
+    }
+
+}
