62ecc25cbefd30b0abff611f080caf0c85d26a3c
Christian Cardinale <cardy.chry@libero.it>
2022-03-13 20:45:54 +0100

    Creazione AddPlaceCuratorActivity e row_add_zona.xml

--- app/src/main/java/com/example/alphatour/AddPlaceCuratorActivity.java
+++ app/src/main/java/com/example/alphatour/AddPlaceCuratorActivity.java
@@ -1,4 +1,4 @@
-package com.example.alphatour.prova;
+package com.example.alphatour;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -6,14 +6,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
-import android.widget.Button;
 import android.widget.EditText;
 
 
-import com.example.alphatour.R;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-public class DashBoardCuratore_seria extends AppCompatActivity {
+public class AddPlaceCuratorActivity extends AppCompatActivity {
 
     FloatingActionButton bottone;
     EditText nomeLuogo;
@@ -26,7 +24,7 @@ public class DashBoardCuratore_seria extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dash_board_curatore_seria);
+        setContentView(R.layout.activity_add_place_curator);
 
         bottone=findViewById(R.id.addLuogo);
         nomeLuogo=findViewById(R.id.textNomeLuogo);
