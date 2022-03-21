1a93c624106e494c92111a998fb01b47b6db4878
Giuseppe Barbone <giuseppe93barbone@gmail.com>
2022-03-21 17:34:18 +0100

    modifica file activity_dash_board.xml, activity_search_prova.xml,DashBoardActivity.java, header_hamburger_prova.xml, ic_search.xml, nav_drawer_menu.xml, nav_header.xml, strings.xml

--- app/src/main/java/com/example/alphatour/DashBoardActivity.java
+++ app/src/main/java/com/example/alphatour/DashBoardActivity.java
@@ -1,14 +1,44 @@
 package com.example.alphatour;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toolbar;
 
-public class DashBoardActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class DashBoardActivity extends AppCompatActivity  {
+
+    private ActionBarDrawerToggle toggle;
+    private Object DrawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dash_board);
+
+        toggle = new ActionBarDrawerToggle(this, (androidx.drawerlayout.widget.DrawerLayout) DrawerLayout, R.string.open,R.string.close);
+        ((DrawerLayout) DrawerLayout).addDrawerListener(toggle);
+        toggle.syncState();
+
+        /*getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        R.id.navView.setNavigationItemSelectedListener();
+        when(it.itemId){
+
+        }*/
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(toggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
