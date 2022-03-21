bde1c70830be5715723652940b2f99f4736ad559
Daniele De Santis <desantisdaniele@hotmail.com>
2022-03-21 17:06:41 +0100

    Creazione dei file WeekViewActivityProva.java, EventEditActivityProva.java, activity_week_view_prova.xml, activity_event_edit_prova.xml e event_cell_prova.xml. Modifica del file AndroidManifest.xml

---
+++ app/src/main/java/com/example/alphatour/EventEditActivityProva.java
@@ -0,0 +1,14 @@
+package com.example.alphatour;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EventEditActivityProva extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event_edit_prova);
+    }
+}
---
+++ app/src/main/java/com/example/alphatour/WeekViewActivityProva.java
@@ -0,0 +1,14 @@
+package com.example.alphatour;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WeekViewActivityProva extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_week_view_prova);
+    }
+}
