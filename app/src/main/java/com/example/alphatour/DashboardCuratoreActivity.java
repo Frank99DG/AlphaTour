ff6190b4268b1b0ffa8594b23dc43de2eb73f820
Desirè Saponaro <83863968+daisysap-1@users.noreply.github.com>
2022-03-22 19:50:25 +0100

    Modifica di : activity_dashboard_curator.xml - DashboardCuratoreActivity.java - strings.xml

--- app/src/main/java/com/example/alphatour/DashboardCuratoreActivity.java
+++ app/src/main/java/com/example/alphatour/DashboardCuratoreActivity.java
@@ -1,14 +1,30 @@
 package com.example.alphatour;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class DashboardCuratoreActivity extends AppCompatActivity {
+    CardView cv1,cv2,cv3,cv4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dashboard_curatore);
+        setContentView(R.layout.activity_dashboard_curator);
+
+        cv1 = (CardView) findViewById(R.id.cv_city);
+        cv2 = (CardView) findViewById(R.id.cv_museum);
+        cv3 = (CardView) findViewById(R.id.cv_show);
+        cv4 = (CardView) findViewById(R.id.cv_monument);
+
+        cv1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"Funonzia",Toast.LENGTH_LONG);
+            }
+        });
     }
 }
