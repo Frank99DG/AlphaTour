e6d1e7d45fc4224e97b24d8458054dbce1eb9a4b
Francesco Pio Di Giorgio <dgfrancesco12@gmail.com>
2022-03-23 17:16:06 +0100

    Modifica dei file strings.xml e LoginActivity

--- app/src/main/java/com/example/alphatour/LoginActivity.java
+++ app/src/main/java/com/example/alphatour/LoginActivity.java
@@ -123,7 +123,7 @@ public class LoginActivity extends AppCompatActivity {
 
                             if (task.isSuccessful()) {
                                     Toast.makeText(LoginActivity.this, "Accesso compiuto", Toast.LENGTH_LONG).show();
-                                    startActivity(new Intent(LoginActivity.this, AddPlaceActivity.class /*AddPlaceActivity.class*//*DashBoardActivity.class*/));
+                                    startActivity(new Intent(LoginActivity.this, UpdateProfileActivity.class /*AddPlaceActivity.class*//*DashBoardActivity.class*/));
                                     loadingBar.setVisibility(View.GONE);
 
                             } else {
