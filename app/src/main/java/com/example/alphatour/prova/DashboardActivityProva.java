2d410656d464bede9f26aa3ffed5bfe771702a03
Desirè Saponaro <83863968+daisysap-1@users.noreply.github.com>
2022-03-28 21:25:19 +0200

    Modifica di : activity_dashboard_prova.xml - DashboardActivityProva.java - strings.xml Aggiunta di : ic_search.png - menu.xml

--- app/src/main/java/com/example/alphatour/prova/DashboardActivityProva.java
+++ app/src/main/java/com/example/alphatour/prova/DashboardActivityProva.java
@@ -1,21 +1,37 @@
 package com.example.alphatour.prova;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 import androidx.cardview.widget.CardView;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import com.example.alphatour.R;
 
+import java.util.List;
+
 public class DashboardActivityProva extends AppCompatActivity {
     CardView cv1,cv2,cv3,cv4;
+    ListView listView;
+    String[] name = {"aaaaa","bbbbb","ccccc","ddddd","eeeee","fffff","ggggg","hhhhh","iiiii"};
+    ArrayAdapter<String> arrayAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard_prova);
+
+        listView = findViewById(R.id.listview);
+        arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,name);
+        listView.setAdapter(arrayAdapter);
+
 
         cv1 = (CardView) findViewById(R.id.cv_route);
         cv2 = (CardView) findViewById(R.id.cv_museum);
@@ -28,5 +44,33 @@ public class DashboardActivityProva extends AppCompatActivity {
                 Toast.makeText(getApplicationContext(),"Funonzia",Toast.LENGTH_LONG);
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu,menu);
+
+        MenuItem menuItem = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) menuItem.getActionView();
+        searchView.setQueryHint("Type here to search");
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+
+                arrayAdapter.getFilter().filter(newText);
+
+                return false;
+            }
+        });
+
+
+        return super.onCreateOptionsMenu(menu);
     }
 }
