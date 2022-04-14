9707b68a184a8f1c5453fc01a01bc00614921090
Matteo Razionale <mat.razionale@gmail.com>
2022-04-14 20:57:34 +0200

    Modifiche dello step 2 del wizard-creazionepercorso: fragment_step2.xml, Step2.java, row_add_zone_creazione_percorso.xml

--- app/src/main/java/com/example/alphatour/wizard/Step2.java
+++ app/src/main/java/com/example/alphatour/wizard/Step2.java
@@ -9,7 +9,11 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.SearchView;
+import android.widget.TextView;
 
 import com.example.alphatour.R;
 import com.stepstone.stepper.Step;
@@ -17,13 +21,57 @@ import com.stepstone.stepper.VerificationError;
 
 public class Step2 extends Fragment implements Step {
 
+    private LinearLayout list_zone;
+    private Button buttonAdd;
+    boolean check = false;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_step2, container, false);
+        boolean check = false;
+        list_zone = view.findViewById(R.id.list_zone);
+        buttonAdd = view.findViewById(R.id.addZone);
+
+        buttonAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+              addZone();
+            }
+        });
+
         return view;
     }
+
+    public void addZone(){
+        View zone = getLayoutInflater().inflate(R.layout.row_add_zone_creazione_percorso,null ,false);
+        TextView textZone = (TextView) zone.findViewById(R.id.textZone);
+
+
+        textZone.setText("Zona 1");
+
+        textZone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                ImageView checked = zone.findViewById(R.id.checked);
+                if(check ==true) {
+                    checked.setVisibility(View.GONE);
+                    check = false;
+                }else {
+                    checked.setVisibility(View.VISIBLE);
+                    check = true;
+                }
+            }
+
+        });
+
+        list_zone.addView(zone);
+    }
+
+
+
 
     @Nullable
     @Override
