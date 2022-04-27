f3cb02bd4f73a042c7f90990d8b20c77a3f6c5aa
Desirè Saponaro <83863968+daisysap-1@users.noreply.github.com>
2022-04-27 22:16:58 +0200

    Modifica di:  Step5.java

--- app/src/main/java/com/example/alphatour/wizardpercorso/Step5.java
+++ app/src/main/java/com/example/alphatour/wizardpercorso/Step5.java
@@ -10,6 +10,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.example.alphatour.HamburgerMenuFragment;
 import com.example.alphatour.R;
@@ -20,6 +21,7 @@ import com.stepstone.stepper.VerificationError;
 public class Step5 extends Fragment  {
     
     private ImageView close_fragment;
+    private LinearLayout closeLayout;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -29,6 +31,7 @@ public class Step5 extends Fragment  {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_step5, container, false);
         close_fragment = view.findViewById(R.id.close_fragment);
+        closeLayout = view.findViewById(R.id.closeLayout);
 
         buttonClick();
         return inflater.inflate(R.layout.fragment_step5, container, false);
@@ -36,12 +39,19 @@ public class Step5 extends Fragment  {
     }
 
     private void buttonClick() {
+
+        closeLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getFragmentManager().beginTransaction().hide(Step5.this).commit();
+            }
+        });
         close_fragment.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 getFragmentManager().beginTransaction().hide(Step5.this).commit();
             }
-            })
-    ;}
+            });
+    }
 
 }
