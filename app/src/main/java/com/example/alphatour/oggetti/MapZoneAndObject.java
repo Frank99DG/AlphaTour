b5fb0869225ada4b6f588f2be013b8497c34aa4e
Francesco Pio Di Giorgio <dgfrancesco12@gmail.com>
2022-05-11 11:52:28 +0200

    Modifica del file MapZoneAndObject

--- app/src/main/java/com/example/alphatour/oggetti/MapZoneAndObject.java
+++ app/src/main/java/com/example/alphatour/oggetti/MapZoneAndObject.java
@@ -5,12 +5,34 @@ import java.util.List;
 
 public class MapZoneAndObject {
 
-    private String zone;
+    private String zone,name,description;
     private List<String> listObj=new ArrayList<String>();
 
-    public MapZoneAndObject(String zone, List<String> listObj) {
+    public MapZoneAndObject() {
+
+    }
+
+    public MapZoneAndObject(String zone, List<String> listObj, String name,String description) {
         this.zone = zone;
         this.listObj = listObj;
+        this.name=name;
+        this.description=description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 
     public String getZone() {
