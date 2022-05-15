b198b13c2ad88ad9da1a1fb3ba3aebe18fefe895
Francesco Pio Di Giorgio <dgfrancesco12@gmail.com>
2022-05-15 18:02:36 +0200

    Modifiche dei file CreateJsonActivity, dialog_step3.xml, activity_create_json.xml. Creazione del file dialog_permission.xml

--- app/src/main/java/com/example/alphatour/CreateJsonActivity.java
+++ app/src/main/java/com/example/alphatour/CreateJsonActivity.java
@@ -1,23 +1,36 @@
 package com.example.alphatour;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.core.content.FileProvider;
 
+import android.Manifest;
+import android.app.Dialog;
+import android.app.DownloadManager;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.FileUtils;
 import android.os.Parcelable;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.alphatour.oggetti.ElementString;
 import com.example.alphatour.oggetti.Zone;
 import com.example.alphatour.oggetti.ZoneChoosed;
 import com.example.alphatour.wizardpercorso.Step4;
+import com.google.android.material.snackbar.Snackbar;
 import com.mxgraph.layout.mxCircleLayout;
 import com.mxgraph.layout.mxIGraphLayout;
 import com.mxgraph.swing.mxGraphComponent;
@@ -50,13 +63,82 @@ import java.util.Objects;
 
 public class CreateJsonActivity extends AppCompatActivity {
 
-    private Button btn;
+    private Button btn,btn1,yesFinal,cancelFinal;
+    private ConstraintLayout layt;
+    private Dialog dialog;
+    private TextView titleDialog,textDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_json);
         btn=findViewById(R.id.button3);
+        layt=findViewById(R.id.layoutJson);
+        btn1=findViewById(R.id.button4);
+
+        dialog=new Dialog(this);
+        dialog.setContentView(R.layout.dialog_permission);
+        dialog.getWindow().setBackgroundDrawable(getResources().getDrawable(R.drawable.backgroun_dialog));
+        dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.WRAP_CONTENT);
+        dialog.setCancelable(false);
+        dialog.getWindow().getAttributes().windowAnimations=R.style.DialogAnimation;
+
+
+        yesFinal= dialog.findViewById(R.id.btn_termina_permission);
+        titleDialog=dialog.findViewById(R.id.titleDialog_permission);
+        textDialog=dialog.findViewById(R.id.textDialog_permission);
+
+        if(checkPermission()){
+            Toast.makeText(this,"Permission granted",Toast.LENGTH_LONG).show();
+        }else{
+            if(ActivityCompat.shouldShowRequestPermissionRationale(CreateJsonActivity.this,Manifest.permission.WRITE_EXTERNAL_STORAGE)){
+                dialog.show();
+                yesFinal.setText("OK");
+
+                titleDialog.setText("Richiesta Permesso");
+                textDialog.setText("Caro utente, la prossima schermata ti chiederà di concedere il permesso di scrittura nella memoria " +
+                        "del tuo dispositivo. Ciò è necessario esclusivamente per scaricare sul tuo dispositivo il percorso che hai creato" +
+                        ", così da poterlo avere sempre a portata di mano. Non accederemo in alcun modo ad altri file nè " +
+                        "divulgeremo dati a terze parti. Ti ringraziamo in anticipo !");
+                textDialog.setTextColor(getResources().getColor(R.color.black));
+
+                yesFinal.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        dialog.dismiss();
+                        requestPermission();
+                    }
+                });
+            }else{
+                requestPermission();
+            }
+
+        }
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Graph<ZoneChoosed,DefaultEdge> graph= Step4.getGraph();
+                JSONExporter<ZoneChoosed, DefaultEdge> exporter = new JSONExporter<>(v -> String.valueOf(v));
+                exporter.setEdgeIdProvider(new IntegerIdProvider<>(1));
+                // ByteArrayOutputStream os = new ByteArrayOutputStream();
+                File file= null;
+                try {
+
+                    File fil=new File(Environment.getExternalStorageDirectory().getAbsolutePath(),"Percorso.json");
+                    FileOutputStream fos= new FileOutputStream(fil);
+                   // file = File.createTempFile("Percorso",".json");
+                    exporter.exportGraph(graph, fos);
+                    fos.close();
+                    Toast.makeText(CreateJsonActivity.this,"Saved to "+getFilesDir()+"/"+ "Percorso.json",Toast.LENGTH_LONG).show();
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -120,6 +202,30 @@ public class CreateJsonActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    private boolean checkPermission(){
+
+        int cameraPermission= ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        return cameraPermission== PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void requestPermission(){
+        int permissionCode=200; //codice definito da me, servirà nel caso in cui serva controllare più permessi
+        ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},permissionCode);
+
+    }
+
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if(grantResults.length>0 && grantResults[0]==PackageManager.PERMISSION_GRANTED ) {
+
+            Toast.makeText(this, "Permission granted", Toast.LENGTH_LONG).show();
+        }else{
+            Toast.makeText(this,"Permission denied",Toast.LENGTH_LONG).show();
+
+        }
     }
 
 }
