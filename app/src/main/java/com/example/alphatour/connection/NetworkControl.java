ef7e9f64be2054376e6b17eb0f251bafa6af0859
Francesco Pio Di Giorgio <dgfrancesco12@gmail.com>
2022-05-18 17:55:33 +0200

    Creazione dei file Receiver, NetworkControl

---
+++ app/src/main/java/com/example/alphatour/connection/NetworkControl.java
@@ -0,0 +1,31 @@
+package com.example.alphatour.connection;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.NetworkInfo;
+
+public class NetworkControl {
+
+    public static String getConnectionStatus(Context context){
+        String status ="";
+
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activityNetwork = cm.getActiveNetworkInfo();
+        if(activityNetwork!=null){
+            if(activityNetwork.getType() == ConnectivityManager.TYPE_WIFI){
+                status="Wifi enabled";
+                return status;
+            }else{
+                if(activityNetwork.getType() == ConnectivityManager.TYPE_MOBILE){
+                    status="Mobile data  enabled";
+                    return status;
+                }
+            }
+        }else{
+            status="No internet is available";
+            return status;
+        }
+        return status;
+    }
+}
---
+++ app/src/main/java/com/example/alphatour/connection/Receiver.java
@@ -0,0 +1,65 @@
+package com.example.alphatour.connection;
+
+import static com.example.alphatour.connection.NetworkControl.getConnectionStatus;
+
+import android.app.Dialog;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.alphatour.LoginActivity;
+import com.example.alphatour.R;
+
+public class Receiver extends BroadcastReceiver {
+
+    private Dialog dialog;
+    private Button yesFinal;
+    private TextView titleDialog,textDialog;
+    private Boolean flag=false;
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        /**chiamata alla connessione*/
+
+        String status=getConnectionStatus(context);
+        dialog= new Dialog(context);
+        dialog.setContentView(R.layout.dialog_permission);
+        dialog.getWindow().setBackgroundDrawable(context.getResources().getDrawable(R.drawable.backgroun_dialog));
+        dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.WRAP_CONTENT);
+        dialog.setCancelable(false);
+        dialog.getWindow().getAttributes().windowAnimations=R.style.DialogAnimation;
+
+        yesFinal = dialog.findViewById(R.id.btn_termina_permission);
+        titleDialog=dialog.findViewById(R.id.titleDialog_permission);
+        textDialog=dialog.findViewById(R.id.textDialog_permission);
+        yesFinal.setText("Riprova");
+        titleDialog.setText("Connessione");
+        textDialog.setText("Connessione internet assente");
+
+
+        yesFinal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                context.startActivity(new Intent(context, LoginActivity.class));
+            }
+        });
+
+        if(status.isEmpty() || status.matches("No internet is available")){
+            //dialog.show();
+            flag=false;
+        }else{
+            flag=true;
+        }
+
+        Toast.makeText(context,status,Toast.LENGTH_LONG).show();
+    }
+
+    public Boolean isConnected() {
+        return flag;
+    }
+}
