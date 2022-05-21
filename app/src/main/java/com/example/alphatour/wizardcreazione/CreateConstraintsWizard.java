51572948665a283676ef77e3141e19a7792bee37
Matteo Razionale <mat.razionale@gmail.com>
2022-05-21 21:13:02 +0200

    Commit per aggiustare Cristian

--- app/src/main/java/com/example/alphatour/wizardcreazione/CreateConstraintsWizard.java
+++ app/src/main/java/com/example/alphatour/wizardcreazione/CreateConstraintsWizard.java
@@ -77,7 +77,7 @@ import java.util.Map;
 
 
 public class CreateConstraintsWizard<zone_list> extends Fragment implements Step, BlockingStep {
-
+a
     //private boolean constraintFlag = false;
     //private boolean listFlag = false;
     private ArrayList<String> zone_list=new ArrayList<>();
